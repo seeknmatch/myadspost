1cee757985a5de7a8027fdeaf5298e25b7f639f7
seeknmatch <admin@seeknmatch.com>
2024-12-08 09:28:59 -0500

    Initial Commit

---
+++ myadspostTests/myadspostTests.swift
@@ -0,0 +1,16 @@
+//
+//  myadspostTests.swift
+//  myadspostTests
+//
+//  Created by Julius Zhu on 12/8/24.
+//
+
+import Testing
+
+struct myadspostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
